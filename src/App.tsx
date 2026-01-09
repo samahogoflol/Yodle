61ed73c1cac8785b6bb18c0520cea170fb732dbd
denys <denis.shmatkov95@gmail.com>
2026-01-09 17:19:43 +0200

    reset server

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.css'
 
 import { BookingProvider } from './store/BookingDetailsContext'
-// import { Routes, Route} from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 
 import ScrollToHashElement from './components/Features/scrollToHashElement'
 import { ScrollToTop } from './components/Features/scrollToTop'
 
-// import HomePage from './pages/HomePage/HomePage'
-// import PublicOffer from './pages/PublicOffer/PublicOffer'
-// import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy'
-// import BookingConfirmed from './pages/BookingConfirmed/BookingConfirmed'
-// import FindYourInstructor from './pages/FindYourInstructor/FindYourInstructor'
-// import SecureCheckout from './pages/SecureCheckout/SecureCheckout'
-// import Page404 from './pages/Page404/Page404'
-// import Layout from './components/Layout/Layout'
+import HomePage from './pages/HomePage/HomePage'
+import PublicOffer from './pages/PublicOffer/PublicOffer'
+import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy'
+import BookingConfirmed from './pages/BookingConfirmed/BookingConfirmed'
+import FindYourInstructor from './pages/FindYourInstructor/FindYourInstructor'
+import SecureCheckout from './pages/SecureCheckout/SecureCheckout'
+import Page404 from './pages/Page404/Page404'
+import Layout from './components/Layout/Layout'
 
 
 function App() {
@@ -24,7 +24,7 @@ function App() {
         <BookingProvider>
           <ScrollToHashElement/>
           <ScrollToTop/>
-          {/* <Routes>
+          <Routes>
            <Route element={<Layout/>}> 
             <Route path='/' element={<HomePage/>}/>
             <Route path='publicOffer' element={<PublicOffer/>}/>
@@ -34,7 +34,7 @@ function App() {
            </Route>
            <Route path='*' element={<Page404/>}/>
            <Route path='bookingConfirmed' element={<BookingConfirmed/>}/>
-          </Routes> */}
+          </Routes>
         </BookingProvider>
     </div>
     </>
